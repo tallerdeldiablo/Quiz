d8cc4596eebc7be8eae84c7e5621a7e6024f0f89
Ricoding <57916204+tallerdeldiablo@users.noreply.github.com>
2021-09-22 04:16:01 -0400

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,45 +1,64 @@
 var timerEl = document.getElementById('time');
 var messageEl = document.getElementById('game-over');
 var startEl = document.querySelector("#myBtn");
-var olEl = document.querySelector("#ol-list");
-var objeto = {answer:"a1", child1:"500", a2:"white", a3:"white"};
+
+var lapreguntaEl = document.querySelector("welcome");
+let botonEL = document.querySelector("button-div");
+let pregunEL = document.querySelector("preguntasAqui");
+
+var objeto = {answer:"a1", child1:"b", a2:"c", a3:"d"};
+
+let questions = ["Inside which HTML element do we put the JavaScript?", "Second","Third"]
+
+let answers = ["<script>", "<js>", "<javascript>" ]
+const choices = [{
+
+  a: 'yes',
+  b: 'no',
+  c: 'no',
+  answer: 1,
+  indd: 1 
+},
+{
+a: 'choice1',
+b: 'choice2',
+c: 'choice2',
+answer: 2,
+indd: 1 
+}];
+
 
 var message =
   'GAME OVER';
 var words = message;
 
-// Timer that counts down from 5
 function countdown() {
   var timeLeft = 21;
 
   var timeInterval = setInterval(function () {
-    // As long as the `timeLeft` is greater than 1
+
     if (timeLeft > 1) {
-      // Set the `textContent` of `timerEl` to show the remaining seconds
+  
       timerEl.textContent = timeLeft + ' seconds remaining,';
           timeLeft--;
           
     } else if (timeLeft === 1) {
-      // When `timeLeft` is equal to 1, rename to 'second' instead of 'seconds'
+  
       timerEl.textContent = timeLeft + ' second remaining';
       timeLeft--;
     } else {
-      // Once `timeLeft` gets to 0, set `timerEl` to an empty string
-     // timerEl.textContent = '';
-      // Use `clearInterval()` to stop the timer
+   
       clearInterval(timeInterval);
       timerEl.textContent = timeLeft + ' Time over';
-      // Call the `displayMessage()` function
+ 
        displayMessage();
     }
   }, 1000);
 }
 
-// Displays the message one word at a time
 function displayMessage() {
   var wordCount = 0;
 
-  // Uses the `setInterval()` method to call a function to be executed every 1000 milliseconds
   var msgInterval = setInterval(function () {
      
     clearInterval(msgInterval);
@@ -53,15 +72,22 @@ function startQuest() {
     var myobj = document.getElementById("myBtn");
     myobj.remove();
 
-    document.getElementById("content-question").innerHTML = "PRIMERA RONDA " + objeto.type;
-      
+   document.getElementById("question").innerHTML = "1" + questions[0];
+
+    var a = document.getElementById("first-screen").innerText = "a" + answers[0];
+  
+    
+    var newdiv = document.createElement("button");
+   
+    pregunEL.append (newdiv)
+
+   
+
         
-     
   
 
 
 }
-
 
 
 startEl.addEventListener("click", function() {  
@@ -71,4 +97,8 @@ startEl.addEventListener("click", function() {
 
 
 });
+
+
+
+
 
