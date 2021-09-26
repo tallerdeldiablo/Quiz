0bddf269e2bb701a4a63830789eadf3d008ea6d5
Ricoding <57916204+tallerdeldiablo@users.noreply.github.com>
2021-09-26 01:02:56 -0400

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,7 @@ var startEl = document.querySelector("#myBtn");
 //var firsScreenEl = document.getElementById("#title-question");
 
 var lapreguntaEl = document.querySelector("welcome");
+var questDivEl = document.getElementById("question");
 var botonEL = document.querySelector("button-div");
 
 var remo = document.getElementById("first-screen");
@@ -14,9 +15,12 @@ var score=0;
 
 var questionnumber = 0;
 
-let questions = ["What is Javascript?", "Inside which HTML element do we put the JavaScript?","What does Javascript"]
 
-let answers = ["A programming language", " a style sheet language ", "HyperText markup language", "<script>", "<js>", "<javascript>" , "Create Structure", "Stilize the web", "Increase interactivity" ]
+
+
+//,"What does Javascript"]
+
+//let questions = [question1, "Inside which HTML element do we put the JavaScript?","What does Javascript"]
 
 let questions = [
   { 
@@ -28,64 +32,75 @@ let questions = [
 },
 { 
   question2: "Inside which HTML element do we put the JavaScript?",
-  choice1: "A programming language",
-  choice2: " a style sheet language ",
+  choice1: "Style sheet language ",
+  choice2: "Programming language",
   choice3:"HyperText markup language",
-  answer: 1
-}];
+  answer: 2
+},
+
+{ question3: "What does Javascript do?", 
+  choice1: "Create Structure",   choice2: "Stilize the web",   choice3:"Increase interactivity",
+  answer: 3
+}
+
+];
 
 
-//GAME OVER
-  function gameover() {
-   
-    
- 
-      var Text = prompt("Please enter your name", "AAA");
-      if (Text != "" && Text != null) {
-        Text = Text.trim (  );
-        location.href = "scores.html";
-    }   
-  }
 
-
-var message =
-  'GAME OVER';
-var words = message;
-
-function countdown(time) {
-  var timeLeft =time;
+//Timer
+  
+function countdown() {
+  var timeLeft =5;
 
   var timeInterval = setInterval(function () {
 
     if (timeLeft > 1) {
   
-      timerEl.textContent = timeLeft + ' seconds remaining,';
+      timerEl.textContent = timeLeft + '  remaining,';
           timeLeft--;
-          
-    } else {
-  
-       clearInterval(timeInterval);
-      timerEl.textContent = timeLeft + ' Time over';
+        
+    } else  if (timeLeft == 0 ) {
+   
+     
+/*
+      clearInterval(timeInterval);
+      timerEl.textContent = timeLeft + ' Time over';*/
+      gameover();
       
-       gameover();
        console.log(remo);
     }
   }, 1000);
 }
 
-
-//GAME OVER
 function gameover() {
    
+    
  
-  var Text = prompt("GAME OVER Please enter your name", "AAA");
+  var Text = prompt("Please enter your name", "AAA");
   if (Text != "" && Text != null) {
     Text = Text.trim (  );
-    location.href = "scores.html"
+    location.href = "scores.html";
 } else {
     document.write ( "Error!" );
 }
 
+
+}
+
+//GAME OVER
+function gameover() {
+   
+  
+  var Text = prompt("GAME OVER Please enter your name", "AAA");
+  if (Text != "" && Text != null) {
+    Text = Text.trim (  );
+    location.href = "scores.html"
+
+
+} /*else {
+    document.write ( "Error!" );
+}
+*/
 
 }
 
@@ -96,7 +111,7 @@ function gameover() {
 startEl.addEventListener("click", function() {  
  
 
-  countdown(65);
+  countdown();
   startQuest();
   
 //llama timer / Start Quiz
@@ -105,83 +120,9 @@ startEl.addEventListener("click", function() {
 });
 
 
-
-
-
-
-
-// ------------START QUIZ----------------
-function startQuest() {
- 
-  var myobj = document.getElementById("myBtn");
- myobj.remove();
-
-   document.getElementById("question").innerHTML = "1" + questions[0];
-
-  
-      
-    let parent = document.getElementById('preguntasAqui');
-    let btn1= document.createElement('button')
-    btn1.id = 'button-div'
-    btn1.textContent = answers[0];
-    parent.insertAdjacentElement('beforebegin', btn1)
-
-
-    
-    let parent2 = document.getElementById('preguntasAqui')
-    let btn2= document.createElement('button')
-    btn2.id = 'button-div2'
-    btn2.textContent = answers[1];
-    parent2.insertAdjacentElement('beforeend', btn2)
-
-    
-    var parent3 = document.getElementById('preguntasAqui')
-    var btn3= document.createElement('button')
-    btn3.id = 'button-div3'
-    btn3.textContent = answers[2];
-    parent3.insertAdjacentElement('beforeend', btn3)
-
-
-        btn1.addEventListener("click", () => {
-          alert(" CORRECT ");
-          btn1.remove();
-          btn2.remove();
-           btn3.remove();
-        score ++;
-        secondQuest();
-        
-        });
-        btn2.addEventListener("click", () => {
-          alert("WRONG!");
-          btn1.remove();
-          btn2.remove();
-          btn3.remove();
-          countdown(countdown-8);
-        secondQuest();
-        
-          });
-          btn3.addEventListener("click", () => {
-          alert("WRONG!");
-          btn1.remove();
-          btn2.remove();
-          btn3.remove();
-        secondQuest();
-        countdown(countdown-8);
-        });
-
-
-}
-
-
-
-
-
 function secondQuest() {
    
-  var myobj = document.getElementById("myBtn");
-  
-     document.getElementById("question").innerHTML = "2" + questions[1];
-  
+   
             
      
      let parent = document.getElementById('preguntasAqui');
@@ -240,6 +181,56 @@ function secondQuest() {
   
   }
  
+
+
+
+var parent = document.getElementById('preguntasAqui');
+
+
+// ------------START QUIZ----------------
+function startQuest() {
+ 
+  var myobj = document.getElementById("myBtn");
+ myobj.remove();
+
+  // document.getElementById("question").innerHTML = "1" + questions[0];
+
+  //document.getElementById("question").innerHTML = questions[0].question1;
+  questDivEl.innerHTML += `<h3> ${questions[0].question1} </h3>`;
+
+  let btn1 = questDivEl.innerHTML += `<button> ${questions[0].choice1} </button>`;
+   // document.createElement('button')
+    
+   let btn2 = questDivEl.innerHTML += `<button> ${questions[0].choice2} </button>`;
+   let btn3 = questDivEl.innerHTML += `<button> ${questions[0].choice3} </button>`;
+ 
+   btn1.addEventListener("click", () => {
+    alert(" CORRECT ");
+      parent.remove();
+        secondQuest();
+      })
+      
+        btn2.addEventListener("click", () => {
+          alert("WRONG!");
+          parent.remove();
+              countdown += countdown -1;
+              secondQuest();
+        
+          });
+          btn3.addEventListener("click", () => {
+          alert("WRONG!");
+          parent.remove();
+          
+        secondQuest();
+        countdown += countdown -1;
+        gameover();
+        });
+}
+
+
+
+
+
   
 //last
 
@@ -252,64 +243,34 @@ function thirdQuest() {
    document.getElementById("question").innerHTML = "3" + questions[2];
 
   
-   let parent = document.getElementById('preguntasAqui');
-   let btn1= document.createElement('button')
-   btn1.id = 'button-div'
-   btn1.textContent = answers[6];
-   parent.insertAdjacentElement('beforebegin', btn1)
-
-
-  
-   let parent2 = document.getElementById('preguntasAqui')
-   let btn2= document.createElement('button')
-   btn2.id = 'button-div2'
-   btn2.textContent = answers[7];
-   parent2.insertAdjacentElement('beforeend', btn2)
-
-  
-   let parent3 = document.getElementById('preguntasAqui')
-   let btn3= document.createElement('button')
-   btn3.id = 'button-div3'
-   btn3.textContent = answers[8];
-   parent3.insertAdjacentElement('beforeend', btn3)
-
-
+   let btn1 = questDivEl.innerHTML += `<button> ${questions[0].choice1} </button>`;
+   // document.createElement('button')
+    
+   let btn2 = questDivEl.innerHTML += `<button> ${questions[0].choice2} </button>`;
+   let btn3 = questDivEl.innerHTML += `<button> ${questions[0].choice3} </button>`;
+ 
    btn1.addEventListener("click", () => {
     alert(" CORRECT ");
-    
-    btn1.remove();
-    btn2.remove();
-    btn3.remove();
-   score++
-   end();
-  
-  });
-  btn2.addEventListener("click", () => {
-    alert("WRONG!");
-    btn1.remove();
-    btn2.remove();
-    btn3.remove();
- 
-   end();
-   
- });
- btn3.addEventListener("click", () => {
-  alert("WRONG!");
- 
-  btn1.remove();
-  btn2.remove();
-  btn3.remove();
- 
- end();
- 
-});
-
-
-
-  
-
+      parent.remove();
+        secondQuest();
+      })
+      
+        btn2.addEventListener("click", () => {
+          alert("WRONG!");
+          parent.remove();
+              countdown += countdown -1;
+              secondQuest();
+        
+          });
+          btn3.addEventListener("click", () => {
+          alert("WRONG!");
+          parent.remove();
+          
+        secondQuest();
+        countdown += countdown -1;
+        gameover();
+        });
 }
-
 
               //end last
 
@@ -325,4 +286,3 @@ function thirdQuest() {
          } 
 
       }
-
